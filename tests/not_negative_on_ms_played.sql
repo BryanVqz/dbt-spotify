e0559fae6f010504a36f3bce152bfef9aa447c4b
Bryan Quezada <bryan.quezada@perficient.com>
2025-05-12 14:52:36 +0000

    deprecating snowflake_contracts test

--- tests/not_negative_on_ms_played.sql
+++ tests/not_negative_on_ms_played.sql
@@ -1,5 +1,5 @@
 -- ms played have a positive amount, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail.
 
-SELECT * FROM {{ ref( 'fct_listening' )}}
+SELECT * FROM {{ ref( 'stg_listening' )}}
 WHERE ms_played < 0
