10f130d009c7f4aeca7602a0f77012924bc7605c
Bryan Quezada <bryan.quezada@perficient.com>
2025-05-15 00:45:15 +0000

    renaming seeds

--- macros/target_properties.sql
+++ macros/target_properties.sql
@@ -8,5 +8,5 @@ select
     last_altered
 
 from {{ database }}.information_schema.tables
-where table_schema = upper('{{ schema }}')
-order by last_altered desc
+--where table_schema = upper('{{ schema }}')
+order by table_schema, last_altered desc
